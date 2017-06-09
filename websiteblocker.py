4a8dd8f720bb28ac0260d738dd57ade29ad2c236
AkshayPatil26 <akshpatil26@gmail.com>
2017-06-09 13:03:52 -0400

    Lec 90 Infinite loop with DATETIME
    used datetime module to track current hours. dt.now()
    created if loop for printing statements asper time.
    This is a setup to modifi Hosts file in windows.

--- websiteblocker.py
+++ websiteblocker.py
@@ -1,9 +1,14 @@
 import time
+from datetime import datetime as dt
 
 host_path=r"C:\Windows\System32\drivers\etc\hosts" #hosts file path where list of links to be blocked is saved
 redirect="127.0.0.1" #redirection to local host
 website_list=["www.facebook.com","facebook.com"] #list of sites to be blocked
 
-while True:
-    print(1)
+while True: #scripts runs all the time since True is passed
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        print("Working Hours")
+    else:
+        print("Party hard")
+
     time.sleep(5) #5 seconds delay
